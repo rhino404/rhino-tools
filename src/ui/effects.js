ac6a1d254c556e97bc1f1ae8e9322d442edb4f35
rhino <rhino404@pm.me>
2025-08-15 22:30:09 -0400

    ryno tools
    moved stats button out of the controls area
    
    improved wrong answer transitons

--- src/ui/effects.js
+++ src/ui/effects.js
@@ -1,16 +1,9 @@
 export function showCorrectEffect(element) {
-  element.style.background = '#d4edda';
-  element.style.transition = 'background 0.4s';
-  setTimeout(() => {
-    element.style.background = '';
-  }, 600);
+  element.classList.add('correct-effect');
+  setTimeout(() => element.classList.remove('correct-effect'), 600);
 }
 
-
 export function showIncorrectEffect(element) {
-  element.style.background = '#f8d7da';
-  element.style.transition = 'background 10s';
-  setTimeout(() => {
-    element.style.background = '';
-  }, 8000);
+  element.classList.add('incorrect-effect');
+  setTimeout(() => element.classList.remove('incorrect-effect'), 600);
 }
